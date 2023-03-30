bdb9a9e096c811c08b74c63231f8e084d64564df
sri12345-del <seena.0196@gmail.com>
2023-03-30 10:20:50 +0530

    domtask4

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let header=document.getElementById("main-header")
+
+header.style.border="solid 3px black"
+
+let item=document.getElementsByClassName("title")
+
+
+item=item[0].style.color="green"
+
+console.log(item)
